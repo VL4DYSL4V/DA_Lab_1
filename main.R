7dc723fc972600a70f9d8d389bfe509cde7892ea
VladyslavArzamastsev <arzik66677@gmail.com>
2022-04-25 15:14:16 +0300

    Added boxes with whiskers

--- main.R
+++ main.R
@@ -1,6 +1,53 @@
 library(ggplot2)
 
 airTrafficCargoStatistics <- read.csv('Air_Traffic_Cargo_Statistics.csv')
-qplot(airTrafficCargoStatistics$Activity.Period, data=airTrafficCargoStatistics, geom='freqpoly')
-qplot(airTrafficCargoStatistics$Cargo.Weight.LBS, data=airTrafficCargoStatistics, geom='freqpoly')
-qplot(airTrafficCargoStatistics$Cargo.Metric.TONS, data=airTrafficCargoStatistics, geom='freqpoly')
+
+buildFrequencyPolygons <- function() {
+  activityPeriod <- qplot(
+    airTrafficCargoStatistics$Activity.Period,
+    data = airTrafficCargoStatistics,
+    geom = 'freqpoly'
+  )
+  cargoWeightLBS <- qplot(
+    airTrafficCargoStatistics$Cargo.Weight.LBS,
+    data = airTrafficCargoStatistics,
+    geom = 'freqpoly'
+  )
+  cargoMetricTons <- qplot(
+    airTrafficCargoStatistics$Cargo.Metric.TONS,
+    data = airTrafficCargoStatistics,
+    geom = 'freqpoly'
+  )
+  return(list(activityPeriod, cargoWeightLBS, cargoMetricTons))
+}
+
+bultWhiskersAndBoxes <- function() {
+  activityPeriod <- ggplot(
+    data = airTrafficCargoStatistics,
+    aes(y = airTrafficCargoStatistics$Activity.Period)
+  ) + geom_boxplot()
+  cargoWeightLBS <- ggplot(
+    data = airTrafficCargoStatistics,
+    aes(y = airTrafficCargoStatistics$Cargo.Weight.LBS)
+  ) +
+    geom_boxplot() +
+    scale_y_continuous(
+      trans = log2_trans(),
+      breaks = trans_breaks("log2", function(x) 2^x),
+      labels = trans_format("log2", math_format(2^.x))
+    )
+  cargoMetricTons <- ggplot(
+    data = airTrafficCargoStatistics,
+    aes(y = airTrafficCargoStatistics$Cargo.Metric.TONS)
+  ) +
+    geom_boxplot() +
+    scale_y_continuous(
+      trans = log2_trans(),
+      breaks = trans_breaks("log2", function(x) 2^x),
+      labels = trans_format("log2", math_format(2^.x))
+    )
+  return(list(activityPeriod, cargoWeightLBS, cargoMetricTons))
+}
+
+buildFrequencyPolygons()
+bultWhiskersAndBoxes()
